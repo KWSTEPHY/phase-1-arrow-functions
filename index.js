45abb0012d612e578a8ecc9d20af8f8dcb78d23c
stephy <stephshiro35@gmail.com>
2024-03-21 02:48:54 +0300

    add

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+divide(2000,100);
+
+
+
+const square = (num) => num * num;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
